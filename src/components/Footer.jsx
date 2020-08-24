8d61bf6435f47e2231534ccf35d4172dba10b865
Diane Candler <dianedcandler@gmail.com>
2020-08-24 12:18:10 -0500

    removed dupe Employee Directory in Footer, passed test

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <div style={{textAlign: 'center', padding: '30px', backgroundColor: '#d2d2d2'}}>
-           <p>&copy; Employee Directory by Diane Candler</p>
+           <p>&copy; Diane Candler</p>
         </div>
     )
 }
