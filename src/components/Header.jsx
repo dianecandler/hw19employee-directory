be9fe5442236826aebf2e639a57e4597c51e897d
Diane Candler <dianedcandler@gmail.com>
2020-08-24 12:13:54 -0500

    creating link for employee directory

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 export default function Header() {
     return (
         <div style={{textAlign: 'center', padding: '30px', backgroundColor: '#d2d2d2'}}>
-            <h1>Employee Directory</h1>
+            <h1>
+            <a
+          className="App-link"
+          href="https://github.com/dianecandler/hw19employee-directory"
+          target="_blank"
+          rel="noopener noreferrer"
+        >Employee Directory
+        </a>
+        </h1>
             <p>This is a Test Driven Development application using Circle CI, Code Climate, and hosting on Heroku.</p>
         </div>
     )
